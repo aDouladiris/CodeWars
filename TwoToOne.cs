cc28a87463b45d4c968db8c5d31c61c9ac3e1094
aDouladiris <58791947+aDouladiris@users.noreply.github.com>
2020-07-09 13:16:28 +0300

    Update TwoToOne.cs

--- TwoToOne.cs
+++ TwoToOne.cs
@@ -1,3 +1,13 @@
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, 
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
